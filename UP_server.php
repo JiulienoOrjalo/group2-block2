349bdddfcf4cae0754181d455d77b3cf9c9ccc44
Jairus Galang <jaigalang.jg@gmail.com>
2021-04-21 20:22:49 +0800

    Fixed bugs

--- UP_server.php
+++ UP_server.php
@@ -1,66 +1,27 @@
 <?php
-// Prepared Statement
 session_start();
-$connection=mysqli_connect("localhost","root","","upang_hub");
 // Check connection
+$connection = mysqli_connect("localhost", "root", "", "upang_hub");
 if (mysqli_connect_errno()) {
-	echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	exit();}
-if (isset($_POST)) {
-	// getting the input from the form
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+}
+if (isset($_POST['login'])) {
+    // getting the input from the form
     $username = $_POST['username'];
     $password = $_POST['password'];
-	// Create a SQL query but this time use a parameter markers, this are used for variable binding
-    $query = "SELECT * FROM tbl_account WHERE username = ? AND password = ?;";
-	// Initialize statement
-    $stmtinit = mysqli_stmt_init($connection);
-	// Prepare the statement, include the initialize statement and the query
-    $preparedStatement = mysqli_stmt_prepare($stmtinit, $query);
-	// Check if the prepared statement failed
-    if (!$preparedStatement) {
-        echo "\nSQL Statement Failed" . mysqli_stmt_error($stmtinit);
+
+	// SQL Statement without Prepared Statement or escape string, Query the SQL statement on the database and get result
+    $sql_query = "SELECT * FROM tbl_account WHERE username = '$username' AND password = '$password' ";
+    $result = mysqli_query($connection, $sql_query);
+    $getResult = mysqli_fetch_assoc($result);
+
+	// use the mysqli_num_rows to determine if the credentials are stored/existing in the database
+    if ($fetchResult = mysqli_num_rows($result) > 0) {
+        echo "<script>alert('Login Success!');document.location='user.php'</script>";
+        $_SESSION['username'] = $username;
     } else {
-		// bind the parameter, by including the initalized statement object, the "ss" are the data types of the inputs
-		// and put now the variable inputs.
-        mysqli_stmt_bind_param($stmtinit, "ss", $username, $password);
-		// after binding, execute it now.
-        mysqli_stmt_execute($stmtinit);
-		// store ther result in a variable
-        $result = mysqli_stmt_get_result($stmtinit);
-		// create a variable with to fetch the result in associative array and find if theres a value of it more than one
-        if ($row = mysqli_fetch_assoc($result) > 0) {
-			// if user and password has been found, it will login.
-			echo "<script>alert('Login Success!');document.location='user.php'</script>";
-			$_SESSION['username'] = $username;
-        } else {
-			// if not, we will dump the data
-            var_dump($query);
-        }
+        echo "<script>alert('Login Failed!');document.location='login.php'</script>";
     }
 }
 ?>
-<?php
-// session_start();
-// $connection=mysqli_connect("localhost","root","","upang_hub");
-// // Check connection
-// if (mysqli_connect_errno()) {
-// 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
-// 	exit();
-// 	}
-// if (isset($_POST['login'])) {
-// 	// getting the users input from the form and storing it without sanitizing it.
-// 	$username=$_POST['username'];
-// 	$password=$_POST['password'];
-
-// 	 $sql_query = "SELECT * FROM tbl_account WHERE username = '$username' AND password = '$password' ";
-//      $result = mysqli_query($connection,$sql_query);
-// 	 $getResult = mysqli_fetch_assoc($result);
-	
-		
-// 	 if($fetchResult = mysqli_num_rows($result) > 0){
-// 		echo "<script>alert('Login Success!');document.location='user.php'</script>";
-// 				$_SESSION['username'] = $username;
-// 	 }else {
-// 		echo "<script>alert('Login Failed!');document.location='login.php'</script>";
-// 	 }
-// 	}?>
